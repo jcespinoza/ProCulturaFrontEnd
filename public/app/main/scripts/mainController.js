4e1c33c062124d22edb0a89559f65c942e0a1a16
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-06-29 14:38:10 -0600

    Fixed mainController's name

--- public/app/main/scripts/mainController.js
+++ public/app/main/scripts/mainController.js
@@ -1,6 +1,6 @@
 /**
  * Created by jcespinoza on 6/21/15.
  */
-angular.module("app").controller("loginController", function($scope){
+angular.module("app").controller("mainController", function($scope){
 
 });
