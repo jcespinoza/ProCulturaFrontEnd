3f176507e6c0e5006629723023a692bec0654478
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-06-29 11:52:34 -0600

    Added preview text

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,7 @@
 /**
  * Created by jcespinoza on 6/21/15.
  */
-angular.module('app', ['ngRoute', 'ngResource']);
-
-angular.module('app').config(function($routeProvider, $locationProvider){
+angular.module('app', ['ngRoute', 'ngResource']).config(function($routeProvider, $locationProvider){
 
     $locationProvider.html5Mode({
         enabled: true,
@@ -14,18 +12,14 @@ angular.module('app').config(function($routeProvider, $locationProvider){
         .when('/', {templateUrl: 'partials/main/view/main', controller: 'mainController'})
         .when('/login',  {templateUrl: 'partials/account/view/login', controller: 'loginController'})
         .when('/signup', {templateUrl: 'partials/account/view/signup', controller: 'signupController'})
-});
-
-angular.module('app').run(function($rootScope, $location){
+}).run(function($rootScope, $location){
     $rootScope.$on('$routeChangeError', function(event, current, previous, rejection){
         if(rejection === 'not authorized'){
             $location.path('/');
         }
         console.log(rejection);
     });
-});
-
-angular.module('app').run(function($rootScope, $location){
+}).run(function($rootScope, $location){
     $rootScope.$on('$routeChangeStart', function(event, next, current){
         console.log(next.templateUrl);
     });
