62c2704e1508740103da1b4e976046b14fd4e721
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-06-30 01:42:30 -0600

    Added binding in login view
    Added ajax call to API from loginController

--- public/app/account/scripts/loginController.js
+++ public/app/account/scripts/loginController.js
@@ -3,11 +3,28 @@
  */
 'use strict';
 angular.module('app').controller('LoginController', function($scope, $http){
+    $scope.user = {};
+    var serverRoute = 'http://localhost:11705';
+
     $scope.checkPasswordLengthIsAcceptable = function(password){
         return password !== undefined && password.length > 8;
     };
 
     $scope.signIn = function(userModel){
-        $http.post('/api/login', userModel)
+        $http.post(serverRoute + '/api/login', userModel).
+            success(function(data, status, headers, config) {
+                console.log('SUCCESS!');
+                console.log(data);
+                console.log(status);
+                console.log(headers);
+                console.log(config);
+            }).
+            error(function(data, status, headers, config) {
+                console.log('ERROR!');
+                console.log(data);
+                console.log(status);
+                console.log(headers);
+                console.log(config);
+            });
     };
 });
