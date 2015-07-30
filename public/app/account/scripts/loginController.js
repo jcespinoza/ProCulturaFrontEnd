a51aba105c04cde9af7d4d6e6a10cddbb93ce410
rafanetow <rafanetow@gmail.com>
2015-07-30 20:51:51 +0200

    Services doesnt work

--- public/app/account/scripts/loginController.js
+++ public/app/account/scripts/loginController.js
@@ -2,9 +2,9 @@
  * Created by jcespinoza on 6/21/15.
  */
 'use strict';
-angular.module('app').controller('LoginController', function($cookies,$scope, $http){
+angular.module('app').controller('LoginController', function($cookies,$scope, $http,HostFactory){
     $scope.user = {};
-    var serverRoute = 'http://localhost:11705';
+  
     $scope.showErrorMessage = false;
     $scope.isProcessing = false;
     $scope.checkPasswordLengthIsAcceptable = function(password){
@@ -15,7 +15,7 @@ angular.module('app').controller('LoginController', function($cookies,$scope, $h
         $scope.showErrorMessage = false;
         $scope.isProcessing = true;
 
-        $http.post(serverRoute + '/api/login', userModel).
+        $http.post(HostFactory.serverName + '/api/login', userModel).
             success(function(data, status, headers, config) {
                 $cookies.put('Token',data.AccessToken);
                 $scope.isProcessing = false;
---
+++ public/app/services/HostFactory.js
@@ -0,0 +1,17 @@
+angular.module('app', [])
+.factory('HostFactory', function() {
+   return {
+   	serverName : "http://localhost:11705"
+   }
+  
+});
+
+
+
+
+
+
+
+
+
+
