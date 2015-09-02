4ae5fd74abe1f2ca66ccd551bd7aad9ba6bffcfe
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-09-01 23:31:45 -0600

    Disregarded changes to EventService

--- public/app/services/EventService.js
+++ public/app/services/EventService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by JuanCarlos on 8/30/2015.
  */
@@ -17,26 +16,4 @@ angular.module('app').service('EventService', ['$http', 'HostFactory', function(
     };
 
     return eventService;
-=======
-angular.module('app').service('EventService', ['$http', 'HostFactory', function($http, HostFactory){
-
-
-    var baseUrl = HostFactory.serverName ;
-    var eventFactory = {};
-
-    eventFactory.createEvent = function(event){
-        event.eventId = 4;
-        event.imgUrl = '';
-        events.push(event);
-        return $http.post(baseUrl + '/api/createEvent', event);
-    };
-
-    eventFactory.getEvents = function(){
-        return $http.get(HostFactory.serverName + '/api/Event');
-
-    };
-
-
-    return eventFactory;
->>>>>>> 7e0f8a8288fac1d98ad7587f99d4ffbf20231261
 }]);
