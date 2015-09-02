3128e873915d925dee46e93e9d4650f3724462b7
rafanetow <rafanetow@gmail.com>
2015-09-02 00:10:24 -0600

    Create IdentityService

--- public/app/services/IdentityService.js
+++ public/app/services/IdentityService.js
@@ -1,3 +1,9 @@
 angular.module('app').service('IdentityService', ['$http', 'HostFactory', function($http, HostFactory)
+	var baseUrl = HostFactory.serverName ;
+	
+	IdentityService.getCurrentUser = function(token){
+				return "";
+	}
 
-	]);
+
+	}]);
