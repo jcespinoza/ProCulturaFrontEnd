2a95ef4624a6f39a9e573164f0ccfc5acb66dda3
rafanetow <rafanetow@gmail.com>
2015-09-02 00:29:15 -0600

    Creating IdentityService but i need know if the way that i call the api is correct

--- public/app/services/IdentityService.js
+++ public/app/services/IdentityService.js
@@ -1,9 +1,8 @@
 angular.module('app').service('IdentityService', ['$http', 'HostFactory', function($http, HostFactory)
 	var baseUrl = HostFactory.serverName ;
 	
-	IdentityService.getCurrentUser = function(token){
-				return "";
+	IdentityService.getCurrentUser = function(token)
+				return  return $http.get(baseUrl + '/api/User',token);
 	}
-
 
 	}]);
