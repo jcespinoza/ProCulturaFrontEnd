00415716eeb982310bf59654565e70a2fa6e242d
rafanetow <rafanetow@gmail.com>
2015-09-07 14:18:56 -0600

    First Try UpdateEventController

--- public/app/events/scripts/UpdateController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Rafael on 07/09/2015.
- */
-angular.module('app').controller('EventDetailsController',["$scope","EventService","$routeParams",
-    function($scope,EventService, $routeParams){
-
-
-
-    }]);
---
+++ public/app/events/scripts/UpdateEventController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Rafael on 07/09/2015.
+ */
+angular.module('app').controller('EventDetailsController',["$scope","EventService","$routeParams",
+    function($scope,EventService, $routeParams){
+
+            EventService.updateEvent($routeParams.event)
+                .success(function(data, status, headers, config) {
+                    toastr.success('Event Created Succesfully');
+                }).
+        error(function(data, status, headers, config) {
+            toastr.error(data);
+        });
+
+    }]);
