e31bc46f09bad5150c84c0aa8a38164ec42874ee
rafanetow <rafanetow@gmail.com>
2015-09-07 13:57:53 -0600

    UpdateController Created

---
+++ public/app/events/scripts/UpdateController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Rafael on 07/09/2015.
+ */
+angular.module('app').controller('EventDetailsController',["$scope","EventService","$routeParams",
+    function($scope,EventService, $routeParams){
+
+
+
+    }]);
