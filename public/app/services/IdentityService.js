d9f21277a09db3cdc9497162cd10aa4feaaa2318
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-09-10 19:34:28 -0600

    Casing and Rafa's crappy function definition bugfix

--- public/app/services/IdentityService.js
+++ public/app/services/IdentityService.js
@@ -1,8 +1,7 @@
-angular.module('app').service('IdentityService', ['$http', 'HostFactory', function($http, HostFactory)
+angular.module('app').service('IdentityService', ['$http', 'HostFactory', function($http, HostFactory){
 	var baseUrl = HostFactory.serverName ;
 	
-	IdentityService.getCurrentUser = function(token)
-				  return $http.get(baseUrl + '/api/User',token);
+	IdentityService.getCurrentUser = function(token){
+		return $http.get(baseUrl + '/api/User',token);
 	}
-
-	}]);
+}]);
