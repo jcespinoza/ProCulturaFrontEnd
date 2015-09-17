c27f7931cc8ac6f627589ee18e09b017cb245bd9
mario ibarra <jmibarrad@gmail.com>
2015-09-16 23:28:25 -0600

    Create Event L10N finished

--- public/js/Spanish.js
+++ public/js/Spanish.js
@@ -7,19 +7,24 @@ var Strings = {
     //CreateView
                 title: "Crear Evento",
                 eventName: "Nombre de Evento",
-                eventNameTooltip: "Ej. Obra Teatral: Helen Keller",
                 shortDescription: "Descripcion Corta",
-                shortDescriptionTooltip: "Breve descripcion del evento",
                 longDescription: "Descripcion",
-                longDescriptionTooltip: "Descripcion completa del evento.",
                 beginDate: "Fecha de Inicio",
                 endDate: "Fecha Culminacion",
                 Location: "Lugar",
                 imageUrl: "URL Imagen",
                 beginTime: "Hora Inicio",
                 endTime: "Hora Culminacion",
-                createButton: "CREAR"
-    //CreateView End
+                createButton: "CREAR",
+                locationTitle: "Localidad",
+        //Tooltips
+                longDescriptionTooltip: "Descripcion completa del evento.",
+                eventNameTooltip: "Ej. Obra Teatral: Helen Keller.",
+                shortDescriptionTooltip: "Breve descripcion del evento.",
+                locationTooltip: "Lugar donde se llevara a cabo el evento.",
+                imageUrlTooltip: "Direccion de la imagen del evento.",
+                beginDateTooltip: "dia/mes/a~o | Ej. 03/10/1994",
+                beginTimeTooltip: "Formato 24h | Ej. 23:59",
 
     //Login
 
