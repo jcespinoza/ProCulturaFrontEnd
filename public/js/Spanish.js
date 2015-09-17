79354b0b21a9557893f56c9f5b18f04d54d97745
mario ibarra <jmibarrad@gmail.com>
2015-09-16 22:51:15 -0600

    L10N started

---
+++ public/js/Spanish.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Light on 16/09/2015.
+ */
+
+var Strings = {
+
+    //CreateView
+                title: "Crear Evento",
+                eventName: "Nombre de Evento",
+                eventNameTooltip: "Ej. Obra Teatral: Helen Keller",
+                shortDescription: "Descripcion Corta",
+                shortDescriptionTooltip: "Breve descripcion del evento",
+                longDescription: "Descripcion",
+                longDescriptionTooltip: "Descripcion completa del evento.",
+                beginDate: "Fecha de Inicio",
+                endDate: "Fecha Culminacion",
+                Location: "Lugar",
+                imageUrl: "URL Imagen",
+                beginTime: "Hora Inicio",
+                endTime: "Hora Culminacion",
+                createButton: "CREAR"
+    //CreateView End
+
+    //Login
+
+    //Login End
+
+    //UpdateView
+
+    //UpdateView End
+};
